27de1e44349dfd3e131f29e8708f7aa2f1527b79
char-lie <5962274+char-lie@users.noreply.github.com>
2016-03-02 22:41:09 +0200

    Implemented Orders find

--- mongo_populate/index.js
+++ mongo_populate/index.js
@@ -15,50 +15,70 @@ var AMOUNT = {
     products: 10000
 };
 
-var ACTIONS = ['populate', 'find'];
+var ACTIONS = ['populate', 'findProducts', 'findOrders'];
 var LIMIT = Number(process.env.LIMIT) || 10;
 assert(LIMIT > 0);
 
 MongoClient.connect(url, function(err, db) {
-  assert(!err);
-  console.log("Connected correctly to server");
-  switch (process.env.ACTION) {
-      case 'populate': 
-            populate(db, AMOUNT).then(function (values) {
-                console.log("Disconnect");
-                db.close();
-            });
-          break;
-      case 'find':
-            var collections = {};
-            find.getCollection(db, COLLECTION.PRODUCT).then(function (collection) {
-                collections[COLLECTION.PRODUCT] = collection;
-                return new Promise(function (fulfill, reject) {
-                    fulfill(collection);
-                });
-            }, function (err) {
-                console.error(err);
-            })
-            .then(find.getCursor, function (err) {
-                console.error(err);
-            }).then(find.showFound.bind(null, LIMIT), function (err) {
-                console.error(err);
-            }).then(find.getInBounds.bind(null, 'size', 100, 1000, collections), function (err) {
-                console.error(err);
-            }).then(find.showFound.bind(null, LIMIT), function (err) {
-                console.error(err);
-            }).then(find.findByCategory.bind(null, 'department', 'Electronics', collections), function (err) {
-                console.error(err);
-            }).then(find.countFound.bind(null), function (err) {
-                console.error(err);
-            }).then(function () {
-                console.log("Disconnect");
-                db.close();
-            });
+assert(!err);
+console.log("Connected correctly to server");
+    var collections = {};
+    switch (process.env.ACTION) {
+        case 'populate': 
+              populate(db, AMOUNT).then(function (values) {
+                  console.log("Disconnect");
+                  db.close();
+              });
             break;
-      default:
-          console.log("ACTION " + process.env.ACTION + " is unavailable. Available are: '" + ACTIONS.join("', '") + "'.");
-          db.close();
-  }
+        case 'findProducts':
+              find.getCollection(db, COLLECTION.PRODUCT)
+              .then(function (collection) {
+                  collections[COLLECTION.PRODUCT] = collection;
+                  return new Promise(function (fulfill, reject) {
+                      fulfill(collection);
+                  });
+              })
+              .then(find.getCursor)
+              .then(find.showFound.bind(null, LIMIT))
+              .then(find.getInBounds.bind(null, 'size', 100, 1000, collections, COLLECTION.PRODUCT))
+              .then(find.showFound.bind(null, LIMIT))
+              .then(find.findByCategory.bind(null, 'department', 'Electronics', collections, COLLECTION.PRODUCT))
+              .then(find.countFound.bind(null))
+              .then(find.getValues.bind(null, 'department', collections, COLLECTION.PRODUCT))
+              .then(function (data) {
+                  console.dir(data);
+              }).then(function () {
+                  console.log("Disconnect");
+                  db.close();
+              }).catch(function (err) {
+                  console.error(err);
+                  console.log("Disconnect");
+                  db.close();
+              });
+              break;
+        case 'findOrders':
+              find.getCollection(db, COLLECTION.ORDER)
+              .then(function (collection) {
+                  collections[COLLECTION.ORDER] = collection;
+                  return new Promise(function (fulfill, reject) {
+                      fulfill(collection);
+                  });
+              })
+              .then(find.getCursor)
+              .then(find.showFound.bind(null, LIMIT))
+              .then(find.getInBounds.bind(null, 'total_sum', 100, 1000, collections, COLLECTION.ORDER))
+              .then(function () {
+                  console.log("Disconnect");
+                  db.close();
+              }).catch(function (err) {
+                  console.error(err);
+                  console.log("Disconnect");
+                  db.close();
+              });
+              break;
+        default:
+            console.log("ACTION " + process.env.ACTION + " is unavailable. Available are: '" + ACTIONS.join("', '") + "'.");
+            db.close();
+    }
 });
 
